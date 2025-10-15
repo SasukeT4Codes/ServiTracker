d287b5ba732467597e7edb9ca1ff88771aba0533
SasukeT4Codes <st4codes@gmail.com>
2025-10-15 16:10:28 -0500

    clase 1

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, saludo
 
 urlpatterns = [
     path('', index, name='index'),
+    path('saludo/', saludo, name='saludo'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 def index(request):
     context = {'name': 'Estudiante UDI'}
     return render(request, 'index.html', context)
+
+def saludo(request):
+    nombre = request.GET.get('nombre')
+    edad = request.GET.get('edad')
+
+    context = {
+        'nombre': nombre,
+        'edad': edad,
+        'has_data': bool(nombre and edad)
+    }
+    return render(request, 'saludo.html', context)
